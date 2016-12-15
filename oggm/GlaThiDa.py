81223c8ed310495324dcbdfa1a06ea4e74b6e4c2
DanielThorlaksson <danni.thor@gmail.com>
2016-12-15 17:44:46 +0100

    GlaThiDa class created with methods to manipulate data. Data ready for Volume vs. A's and Bias vs. A's

---
+++ oggm/GlaThiDa.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import netCDF4
+
+import oggm.cfg as cfg
+from oggm.core.preprocessing import inversion
+
+class GlaThiDa:
+    """A class to store the GlaThiDa data and whith methods to compare GlaThiDa with OGGM output"""
+
+    def __init__(self):
+        self.POINT_LON = None
+        self.POINT_LAT = None
+        self.THICKNESS = None
+        self.RGI_Reg = None
+        self.GlaThiDa_ID = None
+
+    def read_pickle(self, path, RGI_Reg, GlaThiDa_ID):
+        """Reads the pickle file from the RGI GlaThiDa linkage"""
+        name = path + '/' + RGI_Reg + '/' + GlaThiDa_ID + '/' + GlaThiDa_ID + '.p'
+        df = pd.read_pickle(name)
+        self.POINT_LON = df.POINT_LON
+        self.POINT_LAT = df.POINT_LAT
+        self.GTD_THICKNESS = df.THICKNESS
+        self.RGI_Reg = RGI_Reg
+        self.GlaThiDa_ID = GlaThiDa_ID
+        #self.RGI_ID =
+        return self
+
+    def transform(self, gdir):
+        """Transforms the lat long points to a local grid via salem, maybe."""
+        crs = gdir.grid.center_grid
+        self.i, self.j = crs.transform(self.POINT_LON.values, self.POINT_LAT.values, nearest=True)
+        return self
+
+    def Delta_Thickness(self, gdir):
+        """Findes the difference of the oggm model output and the GlaThiDa points"""
+        self.OGGM_THICK = np.zeros(self.GTD_THICKNESS.shape)
+        self.DELTA = np.zeros(self.GTD_THICKNESS.shape)
+
+        # Stuff from Fabi:
+        how = 'per_interpolation'
+        with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
+            topo = nc.variables['topo'][:]
+            mask = nc.variables['glacier_mask'][:]
+
+        grids_file = gdir.get_filepath('gridded_data', div_id=0)
+        with netCDF4.Dataset(grids_file) as nc:
+            vn = 'thickness'
+            if how is not None:
+                vn += '_' + how
+            thick = nc.variables[vn][:]
+
+        thick = np.where(mask, thick, np.NaN)
+
+
+
+        for k in range(self.j.shape[0]):
+            self.OGGM_THICK[k] = thick[self.j[k], self.i[k]]
+            self.DELTA[k] = thick[self.j[k], self.i[k]] - self.GTD_THICKNESS.iloc[k]
+
+        return self
+
+    def volume_and_bias(self, gdir, start=0.9, stop=1.11, step=0.05):
+        """Runs loops to be able to later plot different volumes and biases as a funciton of A \n
+        the three kwargs refer to numpy arrange, that is what values is cfg.A multiplied with"""
+
+        glen_as = cfg.A*np.arange(start, stop, step)
+        volumes = np.zeros(glen_as.shape)
+        thick_diffs = np.zeros([self.GTD_THICKNESS.shape[0], glen_as.shape[0]])
+        for i in range(glen_as.shape[0]):
+            volumes[i], _ = inversion.invert_parabolic_bed(
+                gdir, fs=0., glen_a=glen_as[i], write=True)
+
+            inversion.distribute_thickness(gdir, how='per_interpolation',
+                                           add_slope=False,
+                                           add_nc_name=True)
+
+            self = self.Delta_Thickness(gdir=gdir)
+            thick_diffs[:, i] = self.DELTA
+
+        self.glens_As = glen_as
+        self.volumes = volumes
+        self.biases = np.nansum(thick_diffs, axis=0)
+        return self
+
--- oggm/tests/test_kesselwf.py
+++ oggm/tests/test_kesselwf.py
@@ -75,53 +75,43 @@ class TestKesselWFInvert(unittest.TestCase):
         # climate.local_mustar_apparent_mb(gdir, tstar=t_star, bias=bias)
 
         # OK.
+        # GlaThiDa data manipulation:
+        # Read the oggm data:
+
+        # Read the GlaThiDa data:
+        # TODO: This needs to be some sort of funciton
+
+        from oggm import GlaThiDa
+
+        gtd = GlaThiDa.GlaThiDa()
+        gtd = gtd.read_pickle(path='/home/daniel/Dropbox/dev/data/ttt_2_rgi',
+                              RGI_Reg=str(11), GlaThiDa_ID=str(1970))
+        gtd = gtd.transform(gdir=gdir)
+
         inversion.prepare_for_inversion(gdir)
-        glen_a = cfg.A
-        v, _ = inversion.invert_parabolic_bed(gdir, fs=0., glen_a=glen_a, write=True)
+
+        # ======= The necessary steps to plot a single glacier! =========
+        v, _ = inversion.invert_parabolic_bed(
+            gdir, fs=0., glen_a=cfg.A, write=True)
 
         inversion.distribute_thickness(gdir, how='per_interpolation',
                                        add_slope=False,
                                        add_nc_name=True)
 
-        # GlaThiDa data manipulation:
-        # Read the oggm data:
-        crs = gdir.grid.center_grid
-        how = 'per_interpolation'
-        with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
-            topo = nc.variables['topo'][:]
-            mask = nc.variables['glacier_mask'][:]
+        gtd = gtd.Delta_Thickness(gdir=gdir)
+        # Here it ends
 
-        grids_file = gdir.get_filepath('gridded_data', div_id=0)
-        with netCDF4.Dataset(grids_file) as nc:
-            vn = 'thickness'
-            if how is not None:
-                vn += '_' + how
-            thick = nc.variables[vn][:]
-
-        thick = np.where(mask, thick, np.NaN)
-
-        # Read the GlaThiDa data:
-        # TODO: This needs to be some sort of funciton
-        df = pd.read_pickle('/home/daniel/Dropbox/dev/data/ttt_2_rgi/11/1970/1970.p')
-        gtd = pd.DataFrame()
-        gtd['POINT_LON'] = df.POINT_LON
-        gtd['POINT_LAT'] = df.POINT_LAT
-        gtd['GTD_THICKNESS'] = df.THICKNESS
-        gtd['i'], gtd['j'] = crs.transform(df.POINT_LON.values, df.POINT_LAT.values, nearest=True)
-        gtd['OGGM_THICKNESS'] = np.nan
-        gtd['DELTA'] = np.nan
-
-        for p in gtd.itertuples():
-            gtd.loc[p.Index, 'OGGM_THICKNESS'] = thick[p.j, p.i]
-            gtd.loc[p.Index, 'DELTA'] = thick[p.j, p.i] - p.GTD_THICKNESS
+        # This line runs the inversions as a loop.
+        # gtd.volume_and_bias(gdir=gdir)
 
         gdir.write_pickle(gtd, 'GlaThiDa')
+
 
         # oggm plotting routine:
         from oggm import plot_kesselwf
         import matplotlib.pyplot as plt
 
-        plot_kesselwf.plot_distributed_thickness(gdir, how=how, GTD=True)
+        plot_kesselwf.plot_distributed_thickness(gdir, how='per_interpolation', GTD=True)
 
         #plt.show()
         plt.savefig('/home/daniel/tempfigs/fig_same.png')
@@ -130,3 +120,88 @@ class TestKesselWFInvert(unittest.TestCase):
                                             title_comment='\n Left: OGGM inversion thickness [m] \n Right: OGGM inversion - GlaThiDa[m]')
         #plt.show()
         plt.savefig('/home/daniel/tempfigs/fig_Delta.png')
+
+
+class TestKesselWFInversions(unittest.TestCase):
+
+    def setUp(self):
+        # test directory
+        self.testdir = os.path.join(current_dir, 'tmp_kesselwf')
+        # self.rm_dir() # Uncomment when doing a dry run
+
+        # Init
+        cfg.initialize()
+        cfg.PARAMS['temp_use_local_gradient'] = False
+        cfg.PARAMS['use_multiple_flowlines'] = False
+        cfg.PATHS['dem_file'] = get_demo_file('srtm_oetztal.tif')
+        cfg.PATHS['climate_file'] = get_demo_file('HISTALP_oetztal.nc')
+        cfg.PATHS['cru_dir'] = '~'
+
+    def tearDown(self):
+        pass
+        # self.rm_dir()
+
+    def rm_dir(self):
+        if os.path.exists(self.testdir):
+            shutil.rmtree(self.testdir)
+
+    @requires_py3
+    def test_invert(self):
+        print('number two!')
+        # rgi_file = get_demo_file('rgi_oetztal.shp')
+        # entity = gpd.GeoDataFrame.from_file(rgi_file)
+        # entity = entity.loc[entity.RGIID == 'RGI40-11.00787'].iloc[0]
+
+        # # Load the RGI50 file:
+        entity = gpd.read_file(
+            '/home/daniel/Dropbox/dev/data/rgi50/11_rgi50_CentralEurope.shp')
+        entity = entity.loc[entity.RGIId == 'RGI50-11.00787'].iloc[0]
+
+        gdir = oggm.GlacierDirectory(entity, base_dir=self.testdir)
+        # All the follwing line are only needed for dryruns
+        # gis.define_glacier_region(gdir, entity=entity)
+        # gis.glacier_masks(gdir)
+        # centerlines.compute_centerlines(gdir)
+        # geometry.initialize_flowlines(gdir)
+        # geometry.catchment_area(gdir)
+        # geometry.catchment_width_geom(gdir)
+        # geometry.catchment_width_correction(gdir)
+        # climate.distribute_climate_data([gdir])
+        # climate.mu_candidates(gdir, div_id=0)
+        # hef_file = get_demo_file('mbdata_RGI40-11.00787.csv')
+        # mbdf = pd.read_csv(hef_file).set_index('YEAR')
+        # t_star, bias = climate.t_star_from_refmb(gdir, mbdf['ANNUAL_BALANCE'])
+        # t_star = t_star[-1]
+        # bias = bias[-1]
+        # climate.local_mustar_apparent_mb(gdir, tstar=t_star, bias=bias)
+
+        # OK.
+        # GlaThiDa data manipulation:
+        # Read the oggm data:
+
+        # Read the GlaThiDa data:
+        # TODO: This needs to be some sort of funciton
+
+        from oggm import GlaThiDa
+
+        gtd = GlaThiDa.GlaThiDa()
+        gtd = gtd.read_pickle(path='/home/daniel/Dropbox/dev/data/ttt_2_rgi',
+                              RGI_Reg=str(11), GlaThiDa_ID=str(1970))
+        gtd = gtd.transform(gdir=gdir)
+
+        inversion.prepare_for_inversion(gdir)
+
+        # This line runs the inversions as a loop.
+        gtd.volume_and_bias(gdir=gdir)
+
+        gdir.write_pickle(gtd, 'GlaThiDa')
+
+        # oggm plotting routine:
+        from oggm import plot_kesselwf
+        import matplotlib.pyplot as plt
+
+        plt.plot(gtd.glens_As, gtd.volumes)
+        plt.show()
+        plt.clf()
+        plt.plot(gtd.glens_As, gtd.biases)
+        plt.show()
